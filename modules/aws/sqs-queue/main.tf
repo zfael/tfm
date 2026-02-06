2728d9b99df9c887d7a12cbd7061e2990c0b9fbb
Rafael Bertelli <rafael.p.bertelli@gmail.com>
2026-02-06 11:45:17 -0300

    fix: sqs redrive_policy as string

--- modules/aws/sqs-queue/main.tf
+++ modules/aws/sqs-queue/main.tf
@@ -11,6 +11,17 @@ resource "aws_sqs_queue" "dlq" {
   tags = var.tags
 }
 
+locals {
+  # Determine redrive policy based on DLQ configuration
+  redrive_policy = var.create_dlq ? jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq[0].arn
+    maxReceiveCount     = var.max_receive_count
+    }) : (var.dlq_arn != null ? jsonencode({
+      deadLetterTargetArn = var.dlq_arn
+      maxReceiveCount     = var.max_receive_count
+  }) : null)
+}
+
 resource "aws_sqs_queue" "this" {
   name                       = var.name
   visibility_timeout_seconds = var.visibility_timeout_seconds
@@ -19,22 +30,7 @@ resource "aws_sqs_queue" "this" {
   max_message_size           = var.max_message_size
   receive_wait_time_seconds  = var.receive_wait_time_seconds
 
-  dynamic "redrive_policy" {
-    for_each = var.create_dlq ? [1] : []
-    content {
-      deadLetterTargetArn = aws_sqs_queue.dlq[0].arn
-      maxReceiveCount     = var.max_receive_count
-    }
-  }
-
-  # Use external DLQ if provided
-  dynamic "redrive_policy" {
-    for_each = var.dlq_arn != null && !var.create_dlq ? [1] : []
-    content {
-      deadLetterTargetArn = var.dlq_arn
-      maxReceiveCount     = var.max_receive_count
-    }
-  }
+  redrive_policy = local.redrive_policy
 
   tags = var.tags
 }
